44aaa8e29d9e0c3abbd644be609ce30ee15ae24d
panmatsu <5bji1214@mail.u-tokai.ac.jp>
2015-12-14 21:24:51 +0900

    new

--- app/src/main/java/com/example/matsu/dentaku/CalcActivity.java
+++ app/src/main/java/com/example/matsu/dentaku/CalcActivity.java
@@ -1,7 +1,6 @@
 package com.example.matsu.dentaku;
 
 import android.app.*;
-import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.*;
 import android.preference.PreferenceManager;
@@ -14,13 +13,9 @@ public class CalcActivity extends Activity  {
     int temp;
     int mark;
     int sum;
-    String mList[];
-    ListView listView;
-    TextView text;
-    SharedPreferences data;
-    SharedPreferences.Editor editor;
-    String check;
+    SharedPreferences sp;
     TextView textView;
+    TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
@@ -31,106 +26,111 @@ public class CalcActivity extends Activity  {
 
         //レイアウトをセットする
         setContentView(R.layout.activity_calc);
-        data = PreferenceManager.getDefaultSharedPreferences(this);
 
-        //配列の初期化
-        mList = new String[10];
-
-        //textのセット
         text = (TextView)findViewById(R.id.textView);
         text.setText("00000000");
 
-        listView = (ListView)findViewById(R.id.listView);
-
-        editor = data.edit();
-        for(int i= 9;i>0;i--){
-            editor.putString(mList[i],mList[i]);
-        }
-        mList[0] = Integer.toString(temp);
-        editor.putString(mList[0], mList[0]);
-        editor.apply();
-
-        //Adapterにセット
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,mList);
-        listView.setAdapter(adapter);
-
-        //おためし
-        check = data.getString("test","");
+        //追加
         textView = (TextView)findViewById(R.id.text);
-        textView.setText(check);
+        sp = PreferenceManager.getDefaultSharedPreferences(this);
+        textView.setText(sp.getString("SaveString", null), TextView.BufferType.NORMAL);
 
     }
 
+    //０のとき
     public  void bt0(View v){
         temp *= 10;
         text.setText(Integer.toString(temp));
     }
+
+    //１のとき
     public  void bt1(View v){
         temp = 1 + temp * 10;
         text.setText(Integer.toString(temp));
     }
+
+    //２のとき
     public  void bt2(View v){
         temp = 2 + temp * 10;
         text.setText(Integer.toString(temp));
     }
+
+    //３のとき
     public  void bt3(View v){
         temp = 3 + temp * 10;
         text.setText(Integer.toString(temp));
     }
+
+    //４のとき
     public  void bt4(View v){
         temp = 4 + temp * 10;
         text.setText(Integer.toString(temp));
     }
+
+    //５のとき
     public  void bt5(View v){
         temp = 5 + temp * 10;
         text.setText(Integer.toString(temp));
     }
+
+    //６のとき
     public  void bt6(View v){
         temp = 6 + temp * 10;
         text.setText(Integer.toString(temp));
     }
+
+    //７のとき
     public  void bt7(View v){
         temp = 7 + temp * 10;
         text.setText(Integer.toString(temp));
     }
+
+    //８のとき
     public  void bt8(View v){
         temp = 8 + temp * 10;
         text.setText(Integer.toString(temp));
     }
+
+    //９のとき
     public  void bt9(View v){
         temp = 9 + temp * 10;
         text.setText(Integer.toString(temp));
     }
-    public  void bt01(View v){
+
+    public  void add(View v){          //”＋”を押したとき
         sum = temp;
         temp *= 0;
         mark = 1;
         text.setText(Integer.toString(temp));
     }
-    public  void bt02(View v){
+
+    public  void sub(View v){          //”ー”を押したとき
         sum = temp;
         temp *= 0;
         mark = 2;
         text.setText(Integer.toString(temp));
     }
-    public  void bt03(View v){
+
+    public  void multi(View v){        //”×”を押したとき
         sum = temp;
         temp *= 0;
         mark = 3;
         text.setText(Integer.toString(temp));
     }
-    public  void bt04(View v){
+
+    public  void div(View v){          //”÷”を押したとき
         sum = temp;
         temp *= 0;
         mark = 4;
         text.setText(Integer.toString(temp));
     }
-    public  void bt08(View v){
+
+    public  void clear(View v){        //Clearを押したとき
         temp *= 0;
         text.setText(Integer.toString(temp));
     }
 
-    public  void bt05(View v){
+    public  void equal(View v){        //"＝"を押したとき
         if(mark==1){
             temp += sum;
         }
@@ -143,25 +143,14 @@ public class CalcActivity extends Activity  {
         else if(mark==4){
             temp = sum / temp;
         }
-
-        editor = data.edit();
-        for(int i= 9;i>0;i--){
-            mList[i] = mList[i-1];
-            editor.putString(mList[i],mList[i]);
-        }
-        mList[0] = Integer.toString(temp);
-        editor.putString("test", mList[0]);
-        editor.commit();
-
-        //おためし
-        //check = data.getString("1",mList[0]);
-        textView = (TextView)findViewById(R.id.text);
-        textView.setText(data.getString("test",""));
+        //結果をテキストに表示
+        text.setText(String.valueOf(temp));
 
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,mList);
-        listView.setAdapter(adapter);
-        text.setText(Integer.toString(temp));
+        //Preferenceのテスト
+        textView.setText(String.valueOf(temp));
+        sp = PreferenceManager.getDefaultSharedPreferences(this);
+        sp.edit().putString("SaveString",String.valueOf(temp) ).commit();
 
     }
 
